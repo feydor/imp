46b21bacb5a9fe8c852723fea5cade1268d94a73
feydor <vreyes85739@outlook.com>
2022-06-22 15:24:17 -0500

    remove useless directories for useless os, sorry bill

--- win32/include/controls.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONTROLS_H
-#define CONTROLS_H
-
-/* function prototypes */
-HWND createListView(HWND hwndParent);
-HMENU createMenu(HWND hwnd);
-HWND createStatusBar(HWND hwnd);
-
-#endif
--- win32/include/io.h
+++
@@ -1,9 +0,0 @@
-#ifndef IO_H
-#define IO_H
-
-void getFilename(char *fname, HWND hwnd);
-HBITMAP loadBitmap(char *filename, int width, int height);
-void drawBitmap(HWND hwnd, HBITMAP bmp_todraw);
-void displayWorkingDir(HWND hwnd);
-
-#endif
