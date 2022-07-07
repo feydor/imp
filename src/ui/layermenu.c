e0bb8fff726eece9ce8d9684cdb88e15c3a9da1e
feydor <vreyes85739@outlook.com>
2022-07-06 22:06:04 -0700

    refactor layermenu add/delete layer functions

--- src/ui/layermenu.c
+++ src/ui/layermenu.c
@@ -46,13 +46,21 @@ static void imp_layermenu_add_default_layer(SDL_Renderer *renderer, ImpLayerMenu
     SDL_Surface *surf = SDL_CreateRGBSurface(0, menu->canvas.w, menu->canvas.h, 32, 0, 0, 0, 255);
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surf);
     menu->layers[++menu->selected_layer] = create_imp_layer(menu->canvas, texture);
+    SDL_FreeSurface(surf);
 }
 
 static void imp_layermenu_delete_selected_layer(ImpLayerMenu *menu) {
-    // TODO
+    if (menu->n_layers < 1) {
+        return;
+    }
+    menu->layers = realloc(menu->layers, sizeof(ImpLayer *) * --menu->n_layers);
+    --menu->selected_layer;
 }
 
 bool imp_cursor_over_layer(ImpLayerMenu *lmenu, ImpCursor *cursor) {
+    if (lmenu->n_layers < 1) {
+        return false;
+    }
     ImpLayer *l = lmenu->layers[lmenu->selected_layer];
     return SDL_HasIntersection(&(SDL_Rect){ cursor->x, cursor->y, 1, 1 },
                                &(SDL_Rect){ l->rect.x, l->rect.y, l->rect.w, l->rect.h });
@@ -72,7 +80,6 @@ void imp_layermenu_event(SDL_Renderer *renderer, ImpLayerMenu *menu, SDL_Event *
     switch (e->type) {
         case SDL_MOUSEBUTTONDOWN: {
             if (SDL_HasIntersection(&cursor_rect, &menu->plus_rect)) {
-                printf("clicked PLUS\n");
                 imp_layermenu_add_default_layer(renderer, menu);
             } else if (SDL_HasIntersection(&cursor_rect, &menu->minus_rect)) {
                 imp_layermenu_delete_selected_layer(menu);
@@ -88,24 +95,32 @@ void imp_layermenu_render(SDL_Renderer *renderer, ImpCanvas *canvas, ImpLayerMen
         imp_layer_render(renderer, canvas, menu->layers[i]);
     
     // render the plus/minus buttons
-    int butsize = menu->rect.w / 2;
-    SDL_RenderCopy(renderer, menu->plus_texture, NULL, &(SDL_Rect){ menu->rect.x, menu->rect.y, butsize, butsize });
-    SDL_RenderCopy(renderer, menu->minus_texture, NULL, &(SDL_Rect){ menu->rect.x + butsize, menu->rect.y, butsize, butsize });
+    SDL_RenderCopy(renderer, menu->plus_texture, NULL, &menu->plus_rect);
+    SDL_RenderCopy(renderer, menu->minus_texture, NULL, &menu->minus_rect);
 
     uc r, g, b, a;
-    int ystart = menu->rect.y - butsize;
+    int ystart = menu->rect.y - DEFAULT_BUTTON_SIZE;
     SDL_GetRenderDrawColor(renderer, &r, &g, &b, &a);
-    for (int i = 0; i < menu->n_layers + 1; ++i) {
-        // render stacked rectangles
+    for (int i = 0; i < menu->n_layers; ++i) {
+        // TODO: change to textures
         switch (i % 3) {
         case 0: SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 255); break;
         case 1: SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 255); break;
         case 2: SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 255); break;
         }
-        SDL_RenderFillRect(renderer, &(SDL_Rect){menu->rect.x,
-                                                 ystart - i * menu->rect.h,
-                                                 menu->rect.w,
-                                                 menu->rect.h});
+
+        SDL_Rect dest = (SDL_Rect){menu->rect.x,
+                                   ystart - i * menu->rect.h,
+                                   menu->rect.w,
+                                   menu->rect.h};
+        SDL_RenderFillRect(renderer, &dest);
+
+        if (i == menu->selected_layer) {
+            SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 200);
+            SDL_RenderFillRect(renderer, &dest);
+            SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
+        }
     }
     SDL_SetRenderDrawColor(renderer, r, g, b, a);
 }
